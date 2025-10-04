b0699ea90d05bf5fd67fa304cf9fb4dc2d3c7967
ruihackit <ruihackit@gmail.com>
2025-10-04 15:21:14 +0900

    initial commit (helloworld)

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
